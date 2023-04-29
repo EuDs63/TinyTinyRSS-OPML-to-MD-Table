75dbc8b2a2c046adf2ad0e494ad2007a13a9d531
EuDs63 <erica23187@gmail.com>
2023-04-29 23:10:23 +0800

    尝试使用GithubActions

--- auto.py
+++ auto.py
@@ -2,12 +2,19 @@ import xml.etree.ElementTree as ET
 import requests
 import json
 import os
+from datetime import date
 
 # 路径
 root_path = os.path.dirname(__file__)
-config_path = os.path.join(root_path,'config.json')
-with open(config_path,'r') as f:
-    config = json.load(f)
+
+# 读取config
+## 从config.json中读取
+# config_path = os.path.join(root_path,'config.json')
+# with open(config_path,'r') as f:
+#     config = json.load(f)
+## 从secret中读取
+secrets_json = os.environ['MY_CONFIG_JSON']
+config = json.loads(secrets_json)
 baseUrl = config['baseUrl']
 user = config['user']
 password = config['password']
@@ -23,11 +30,16 @@ session_id = response.json()['content']['session_id']
 #构造获取 OPML 文件的请求，附带 Session ID
 opmlUrl = f"{baseUrl}backend.php?op=opml&method=export"
 response = requests.get(opmlUrl, cookies={'ttrss_sid': session_id})
-OpmlPath = os.path.join(root_path,'feed.opml')
+
+# 获取当前日期
+today = date.today()
+today_str = today.strftime("%Y-%m-%d")
+OpmlName = f"feed_{today_str}.opml"
+OpmlPath = os.path.join(root_path,OpmlName)
 with open(OpmlPath, 'wb') as f:
     f.write(response.content)
 
-# 解析opml文件，并输出为Markdown文件
+# 解析opml文件
 tree = ET.parse(OpmlPath)
 root = tree.getroot()
 
@@ -52,7 +64,9 @@ md_table = "| Title | Feed URL | Html URL|\n| --- | --- | --- |\n"
 for outline in outlines:
     md_table += f"| {outline['title']} | {outline['feed_url']} | {outline['html_url']}|\n"
 
-MarkdownPath = os.path.join(root_path,'Feed.md')
+# 输出为Markdown文件
+MarkdownName = f"feed_{today_str}.md"
+MarkdownPath = os.path.join(root_path,MarkdownName)
 with open(MarkdownPath,'w',encoding='utf-8') as f:
     f.write(md_table)
 
